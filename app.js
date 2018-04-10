c4aed7520b9e0ab6bcf8a9708b14cbfb13eba54d
wuruthai <irkenonur@gmail.com>
2018-04-10 23:53:38 +0300

    version-2 css&html fixed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function openNav() {
-	let nav__menu_id = document.getElementById( "nav__menu" );
-	let icon_id = document.getElementById( "icon" );
-	if ( nav__menu_id.className === "nav__menu" ) {
-		nav__menu_id.className += " active";
-		icon_id.innerHTML = "&#x2715";
-	} else {
-		nav__menu_id.className = "nav__menu";
-		icon_id.innerHTML = "&#9776";
-	}
+  let nav__menu_id = document.getElementById("nav__menu");
+  let icon_id = document.getElementById("icon");
+  if (nav__menu_id.className === "nav__menu") {
+    nav__menu_id.className += " active";
+    icon_id.innerHTML = "&#x2715";
+  } else {
+    nav__menu_id.className = "nav__menu";
+    icon_id.innerHTML = "&#9776";
+  }
 }
